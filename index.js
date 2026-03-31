94d6bf41f5e977c045e71c3bd30665f2d3dcb16a
mahirTheCoder <mahirchowdhury518@gmail.com>
2026-03-31 14:19:58 +0600

    controller add

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const router = express.Router()
 
 const app = express()
 const port = 8000
 app.use(express.json());
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
+mongoose.connect('mongodb+srv://Auths:XxtE9nw9V3Jo0lRw@cluster0.mjewgzf.mongodb.net/routing?appName=Cluster0')
   .then(() => console.log('Connected!'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
