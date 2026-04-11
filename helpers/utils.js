cffc76936f978f75bed432a4905ae51a9f8d5b0b
mahirTheCoder <mahirchowdhury518@gmail.com>
2026-04-11 22:51:28 +0600

    email transporter

--- helpers/utils.js
+++ helpers/utils.js
@@ -102,7 +102,19 @@ function hashToken(token) {
 }
 
 // Setup email transporter
-x
+function getEmailTransporter() {
+    return nodemailer.createTransport({
+        host: process.env.EMAIL_HOST || 'smtp.gmail.com',
+        port: process.env.EMAIL_PORT || 587,
+        secure: process.env.EMAIL_SECURE === 'true',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD
+        }
+    });
+}
+
+
 
 module.exports = { 
     isValidEmail,
